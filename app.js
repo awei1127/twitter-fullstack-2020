e9fffe62c695f8e6875ef2c3b1ab38cfbf6b76e7
Wei <awei1127@hotmail.com>
2023-06-23 23:54:07 +0800

    disable multer

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(methodOverride('_method'))
-app.use('/upload', express.static(path.join(__dirname, 'upload')))
+// app.use('/upload', express.static(path.join(__dirname, 'upload')))
 app.use(session({
   secret: process.env.SESSION_SECRET || 'ThisMySecret',
   resave: false,
